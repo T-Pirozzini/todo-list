c92c35e97478aa4990dfcf624c8069d516b29707
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-06-29 18:06:26 -0700

    factory function created, linked up pages

--- app.js
+++ app.js
@@ -9,12 +9,22 @@ const createTodo = (title, description, dueDate, priority) => {
 
 let todo1 = createTodo('Feed Uno', 'Our furry friend gets feral when hungry', 'July 1, 2022', 'high')
 
-console.log(todo1)
+// console.log(todo1)
+
+const title = document.getElementById('title')
+console.log(title)
 
 const addTodo = (e) => {
   e.preventDefault()
-  console.log('CLICK')
+  console.log(todo1)
+  title.textContent = todo1.title
+  console.log(title.textContent)
+  // title.value == 'value'
+  // console.log(output)
+  // console.log(title.value)
 }
+
+console.log(title.textContent)
 
 const inputBtn = document.querySelector('.input-btn')
 console.log(inputBtn)
