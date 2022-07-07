d171b5a8b7915299d3a345e06e8657955e2b1781
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-07-06 17:26:39 -0700

    set up prettier

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // query/id selectors
-const title = document.getElementById('title')
-const inputBtn = document.querySelector('.input-btn')
-const list = document.querySelector('.list')
-const form = document.querySelector('form')
+const title = document.getElementById("title");
+const inputBtn = document.querySelector(".input-btn");
+const list = document.querySelector(".list");
+const form = document.querySelector("form");
 
 // todo constructor
 const createTodo = (title, description, dueDate, priority) => {
@@ -10,104 +10,109 @@ const createTodo = (title, description, dueDate, priority) => {
     title,
     description,
     dueDate,
-    priority
-  }
+    priority,
+  };
 };
 
 // instantiate todo list array
-let todos = []
+let todos = [];
 
 // Add a test todo
-todos.push(createTodo('Feed Uno', 'Our furry friend gets feral when hungry', 'July 1, 2022', 'high'))
+todos.push(
+  createTodo(
+    "Feed Uno",
+    "Our furry friend gets feral when hungry",
+    "July 1, 2022",
+    "high"
+  )
+);
 
 // Remove todo
 const removeTodo = (e) => {
-  e.currentTarget.parentElement.parentElement.remove()
-}
+  e.currentTarget.parentElement.parentElement.remove();
+};
 // Complete todo
 const completeTodo = (e) => {
-  const complete = document.querySelector('.completed-container')  
-  e.currentTarget.classList.add('completed')  
-  complete.appendChild(e.currentTarget.parentElement.parentElement)
-  e.currentTarget.innerHTML = null
-}
+  const complete = document.querySelector(".completed-container");
+  e.currentTarget.classList.add("completed");
+  complete.appendChild(e.currentTarget.parentElement.parentElement);
+  e.currentTarget.innerHTML = null;
+};
 
 // add a new todo to todos array
 const addTodo = (e) => {
-  e.preventDefault() 
+  e.preventDefault();
 
-  let priorityText = ""
+  let priorityText = "";
   if (priority.value == 1) {
-    priorityText = "low" 
+    priorityText = "low";
   }
   if (priority.value == 2) {
-    priorityText = "medium" 
+    priorityText = "medium";
   }
   if (priority.value == 3) {
-    priorityText = "high" 
+    priorityText = "high";
   }
-  
+
   // push to todos array - not necessary, but might be useful later
-  todos.push(createTodo(title.value, description.value, duedate.value, priorityText))  
-  
+  todos.push(
+    createTodo(title.value, description.value, duedate.value, priorityText)
+  );
+
   // create current todo
-  const currentTodo = createTodo(title.value, description.value, duedate.value, priorityText)
+  const currentTodo = createTodo(
+    title.value,
+    description.value,
+    duedate.value,
+    priorityText
+  );
 
   // create required elements
-  const ul = document.createElement('ul')
-  const titleLi = document.createElement('li')
-  const descriptionLi = document.createElement('li')
-  const duedateLi = document.createElement('li')
-  const priorityLi = document.createElement('li')
-  const complete = document.createElement('span')
-  const remove = document.createElement('span')
-  const div = document.createElement('div')
-  
+  const ul = document.createElement("ul");
+  const titleLi = document.createElement("li");
+  const descriptionLi = document.createElement("li");
+  const duedateLi = document.createElement("li");
+  const priorityLi = document.createElement("li");
+  const complete = document.createElement("span");
+  const remove = document.createElement("span");
+  const div = document.createElement("div");
+
   // add values to elements
-  titleLi.textContent = "Title: " + currentTodo.title
-  descriptionLi.textContent = "Description: " + currentTodo.description
-  duedateLi.textContent = "Deadline: " + currentTodo.dueDate   
-  priorityLi.textContent = `Priority: ${currentTodo.priority}`
-  div.classList.add('update')
+  titleLi.textContent = "Title: " + currentTodo.title;
+  descriptionLi.textContent = "Description: " + currentTodo.description;
+  duedateLi.textContent = "Deadline: " + currentTodo.dueDate;
+  priorityLi.textContent = `Priority: ${currentTodo.priority}`;
+  div.classList.add("update");
 
   //completed task button
-  complete.textContent = "check_circle"
-  complete.classList.add('material-symbols-outlined', 'complete')
+  complete.textContent = "check_circle";
+  complete.classList.add("material-symbols-outlined", "complete");
   complete.addEventListener("click", (e) => {
-    completeTodo(e)
-  })
+    completeTodo(e);
+  });
 
   //remove task button
-  remove.textContent = "delete"
-  remove.classList.add('material-symbols-outlined', 'remove')
+  remove.textContent = "delete";
+  remove.classList.add("material-symbols-outlined", "remove");
   remove.addEventListener("click", (e) => {
-    removeTodo(e)
-  })
+    removeTodo(e);
+  });
 
   // append elements to ul
-  ul.appendChild(titleLi)
-  ul.appendChild(descriptionLi)
-  ul.appendChild(duedateLi)
-  ul.appendChild(priorityLi)
-  div.appendChild(complete)
-  div.appendChild(remove)
-  ul.appendChild(div)  
-  list.appendChild(ul)
+  ul.appendChild(titleLi);
+  ul.appendChild(descriptionLi);
+  ul.appendChild(duedateLi);
+  ul.appendChild(priorityLi);
+  div.appendChild(complete);
+  div.appendChild(remove);
+  ul.appendChild(div);
+  list.appendChild(ul);
 
   // reset the form
-  form.reset();  
-}
+  form.reset();
+};
 
 // add todo on submit 'click'
-inputBtn.addEventListener('click', (e) => {
-  addTodo(e)  
-})
-
-
-
-
-
- 
-
-
-
+inputBtn.addEventListener("click", (e) => {
+  addTodo(e);
+});
